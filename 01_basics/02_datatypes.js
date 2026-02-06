28ad8b8adb314371a05930e8ee221a1fd7c53aff
monikasharma5097-cse <monikasharma5097@gmail.com>
2026-02-06 11:23:10 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,3 +6,26 @@ console.log(3 + 3)
      // code readability should be high
 
 console.log("Monika")
+
+
+
+let name ="Monika"
+let age = 21
+let isLoggedIn = false 
+
+
+// number  =2 to power 53
+// bigint
+// string = ""
+// boolean = true/false 
+// null = standalone value 
+// undefined = 
+// symbol = unique
+
+
+// object 
+
+ console.log(typeof undefined) // undefined 
+ console.log(typeof null) // object
+
+ 
