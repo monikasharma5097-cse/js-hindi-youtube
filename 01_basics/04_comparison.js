5c0eb0c4f1eee064506e90e9d6e294167014d672
monikasharma5097-cse <monikasharma5097@gmail.com>
2026-02-07 16:29:30 +0000

    comparison.js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 ==1);
+//console.log(2 != 1);
+
+
+//console.log( "2" > 1);
+//console.log("02 > 1")
+
+
+
+//console.log(null > 0);
+//console.log(null == 0);  // comparisons and equality works differ
+//console.log(null >= 0);
+
+
+console.log(undefined ==0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+
+console.log("2" === 2);
