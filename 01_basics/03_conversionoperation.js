84be6960c0e0bf2ebe1f22e14f1511254a38ffc6
monikasharma5097-cse <monikasharma5097@gmail.com>
2026-02-07 10:26:41 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,25 @@
+let score = "monika"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1; false = 0
+
+let isLoggedIn = "monika"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 = true; 0 = false
+// "" = false; "monika" = true
+
+let someNumber = 33
+
+let stringNumber = String( someNumber)
+console.log( typeof stringNumber);
